6db899bcd7091beb84556c6d2c058ec40b694e69
Zac Smith <zrs3141592@gmail.com>
2020-08-16 22:59:16 -0600

    use async/await

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,30 +1,28 @@
 const express = require('express')
 const fs = require('fs')
+const fsPromises = require('fs').promises
 const datafile = 'server/data/clothing.json'
 const router = express.Router()
 
 /* GET all clothing */
-router.route('/').get(function (req, res) {
-  getClothingData((err, data) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('Returning clothing data')
-      res.send(data)
-    }
-  })
+router.route('/').get(async function (req, res) {
+  try {
+    let data = await getClothingData()
+    console.log('returning data')
+    res.send(data)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+
   console.log('Doing more work')
 })
 
-const getClothingData = (callback) => {
-  fs.readFile(datafile, 'utf8', (err, data) => {
-    if (err) {
-      callback(err, null)
-    } else {
-      let clothingData = JSON.parse(data)
-      callback(null, clothingData)
-    }
-  })
+const getClothingData = async () => {
+  let rawData = await fsPromises.readFile(datafile, 'utf8')
+  let clothingData = JSON.parse(rawData)
+
+  console.log(clothingData)
+  return clothingData
 }
 
 module.exports = router
