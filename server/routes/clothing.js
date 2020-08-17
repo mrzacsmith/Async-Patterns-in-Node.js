04ab33d599d8e87d322f7721831f52769e06adc0
Zac Smith <zrs3141592@gmail.com>
2020-08-16 21:14:24 -0600

    using callback

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const fs = require('fs');
-const datafile = 'server/data/clothing.json';
-const router = express.Router();
+const express = require('express')
+const fs = require('fs')
+const datafile = 'server/data/clothing.json'
+const router = express.Router()
 
 /* GET all clothing */
-router.route('/')
-  .get(function(req, res) {
+router.route('/').get(function (req, res) {
+  getClothingData((err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Returning clothing data')
+      res.send(data)
+    }
+  })
+  console.log('Doing more work')
+})
 
-    let rawData = fs.readFileSync(datafile, 'utf8');
-    let clothingData = JSON.parse(rawData);
+const getClothingData = (callback) => {
+  fs.readFile(datafile, 'utf8', (err, data) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      let clothingData = JSON.parse(data)
+      callback(null, clothingData)
+    }
+  })
+}
 
-    res.send(clothingData);
-
-  });
-
-module.exports = router;
+module.exports = router
